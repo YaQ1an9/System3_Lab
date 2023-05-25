7bea55a806491998fe5c102faaab73107af1db65
YaQ1an9 <1185365215@qq.com>
2023-05-25 19:03:00 +0800

    Begin Lab5 Change getpid.c syscall.h

--- sys3_lab3/user/syscall.h
+++ sys3_lab3/user/syscall.h
@@ -4,6 +4,11 @@
 #define SYS_WRITE   64
 #define SYS_GETPID  172
 
+#define SYS_MUNMAP   215
+#define SYS_CLONE    220 // fork
+#define SYS_MMAP     222
+#define SYS_MPROTECT 226
+
 struct pt_regs {
     unsigned long regs[32];
     unsigned long sepc;
